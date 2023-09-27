644062a410fced8f6b1125dd603096eb5939fc81
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-09-27 14:50:32 +0200

    Create AboutText.jsx

---
+++ src/Pages/helpers/AboutText.jsx
@@ -0,0 +1,7 @@
+export default function AboutText() {
+  return (
+  <><p>Ich bin Julia Breitenbruch,  IT-begeistert mit einem starken Interesse an der Lösung komplexer Probleme und der Umsetzung kreativer Ideen.</p> <p>Ursprünglich in klinischen Auftragsinstituten tätig, habe ich in den letzten Jahren meine Zeit und Energie in intensives Selbststudium investiert, um meine Kenntnisse in verschiedenen IT-Bereichen, einschließlich Frontend-Entwicklung und Data Science, kontinuierlich zu erweitern.</p>
+<p>
+Wenn Sie jemanden suchen, der nicht nur technische Fähigkeiten, sondern auch Begeisterung und Engagement  mitbringt, dann bin ich bereit, meinen Beitrag zu leisten und gemeinsam mit Ihrem Team großartige Projekte umzusetzen.</p></>
+)
+}
