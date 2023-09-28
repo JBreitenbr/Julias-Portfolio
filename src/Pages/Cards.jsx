ff75792b40fe06ec0ca4fdfa453fb40b24bb7ba0
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-09-28 12:20:56 +0200

    Update Cards.jsx

--- src/Pages/Cards.jsx
+++ src/Pages/Cards.jsx
@@ -1,7 +1,7 @@
 function Cards(props){
   return (  <div className="card project-card-view">
 <h5 className="card-title">{props.title}</h5>
-<a href={props.demoUrl} className="card-pic"> <img className="card-img-top img-custom" src={props.img} alt={props.alt}/></a>
+<a href={props.demoUrl} className="card-pic" target="_blank"> <img className="card-img-top img-custom" src={props.img} alt={props.alt}/></a>
   <div className="card-body">
     <a href={props.demoUrl} className="btn btn-primary" style={{marginRight:"5px"}}>Demo</a>
     <a href={props.githubUrl} className="btn btn-secondary">Source</a>
