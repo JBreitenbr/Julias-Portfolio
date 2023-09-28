27f5184801c8e4fb9cfe1a9748992aabc3fb2a5c
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-09-28 12:23:00 +0200

    Update Cards.jsx

--- src/Pages/Cards.jsx
+++ src/Pages/Cards.jsx
@@ -3,8 +3,8 @@ function Cards(props){
 <h5 className="card-title">{props.title}</h5>
 <a href={props.demoUrl} className="card-pic" target="_blank"> <img className="card-img-top img-custom" src={props.img} alt={props.alt}/></a>
   <div className="card-body">
-    <a href={props.demoUrl} className="btn btn-primary" style={{marginRight:"5px"}}>Demo</a>
-    <a href={props.githubUrl} className="btn btn-secondary">Source</a>
+    <a href={props.demoUrl} className="btn btn-primary" target="_blank" style={{marginRight:"5px"}}>Demo</a>
+    <a href={props.githubUrl} className="btn btn-secondary" target="_blank">Source</a>
   </div>
 </div>)};
 export default Cards;
