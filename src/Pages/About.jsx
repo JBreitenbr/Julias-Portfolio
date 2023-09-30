2fc18da93c8c35a0d8069cdd62fb77bcf887c356
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-09-30 20:19:21 +0200

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,11 +3,11 @@ import AboutText from "./helpers/AboutText.jsx";
 import {Footer} from './Footer';
 export const About = () => {
   return (
-   <div className="about-div">
+   <></><div className="about-div">
      <div>
  <div className="about-container"><h2 className="about">Über mich</h2>
    <AboutText/>
       </div>
-   </div><Footer/></div>
+   </div></div><Footer/></>
   );
 };
